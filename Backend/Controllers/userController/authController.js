d69c89649a917bff321173872c162ad75254bdf4
i202417 <95436786+Ammar123890@users.noreply.github.com>
2023-05-19 10:32:22 +0500

    Forgot password

--- Backend/Controllers/userController/authController.js
+++ Backend/Controllers/userController/authController.js
@@ -67,115 +67,150 @@ const forgotPassword = (req, res) => {
 
   // Check if the email exists in the user collection
   User.findOne({ email: email })
-      .then((user) => {
-          if (!user) {
-              // Email does not exist
-              return res.status(400).send({ message: 'Email not found' });
-          }
+    .then((user) => {
+      if (!user) {
+        // Email does not exist
+        return res.status(400).send({ message: 'Email not found' });
+      }
 
-          // Generate a temporary OTP
-          const otp = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
+      // Generate a temporary OTP
+      const otp = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
 
-          // Save the OTP in the PasswordResetToken collection
-          const resetToken = new PasswordResetToken({
-              user: user._id,
-              otp: otp,
-              expiresAt: new Date(Date.now() + 600000), // OTP expires in 10 minutes
-          });
+      // Save the OTP in the PasswordResetToken collection
+      const resetToken = new PasswordResetToken({
+        user: user._id,
+        otp: otp,
+        expiresAt: new Date(Date.now() + 600000), // OTP expires in 10 minutes
+      });
 
-          resetToken
-              .save()
-              .then(() => {
-                  // Send the OTP to the user's email
-                  sendOtpByEmail(email, otp);
+      resetToken
+        .save()
+        .then(() => {
+          // Send the OTP to the user's email
+          sendOtpByEmail(email, otp);
 
-                  res.status(200).send({ message: 'OTP sent to email' });
-              })
-              .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
-      })
-      .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+          res.status(200).send({ message: 'OTP sent to email' });
+        })
+        .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+    })
+    .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
 };
 
 
 function sendOtpByEmail(email, otp) {
   // Configure the email transporter
   const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
-      },
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
   });
 
   // Configure the email options
   const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: 'Password Reset OTP',
-      text: `Your password reset OTP is: ${otp}`,
+    from: process.env.EMAIL_USERNAME,
+    to: email,
+    subject: 'Password Reset OTP',
+    text: `Your password reset OTP is: ${otp}`,
   };
 
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          console.log(error);
-      } else {
-          console.log('Email sent: ' + info.response);
-      }
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
   });
 }
 
 
 
 
-const resetPassword = (req, res) => {
-  const { email, otp, newPassword } = req.body;
+// const resetPassword = (req, res) => {
+//   const { email, otp, newPassword } = req.body;
 
-  // Find the user based on the email and OTP
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        // User not found or invalid OTP
-        return res.status(400).send({ message: 'Invalid email or OTP' });
-      }
+//   // Find the user based on the email and OTP
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (!user) {
+//         // User not found or invalid OTP
+//         return res.status(400).send({ message: 'Invalid email or OTP' });
+//       }
 
-      // Find the password reset token in the collection
-      PasswordResetToken.findOne({ user: user._id, otp: otp })
-        .then((resetToken) => {
-          if (!resetToken) {
-            // Invalid OTP
-            return res.status(400).send({ message: 'Invalid OTP' });
-          }
+//       // Find the password reset token in the collection
+//       PasswordResetToken.findOne({ user: user._id, otp: otp })
+//         .then((resetToken) => {
+//           if (!resetToken) {
+//             // Invalid OTP
+//             return res.status(400).send({ message: 'Invalid OTP' });
+//           }
 
-          // Check if the OTP has expired
-          if (resetToken.expiresAt < Date.now()) {
-            // OTP has expired
-            return res.status(400).send({ message: 'OTP has expired' });
-          }
+//           // Check if the OTP has expired
+//           if (resetToken.expiresAt < Date.now()) {
+//             // OTP has expired
+//             return res.status(400).send({ message: 'OTP has expired' });
+//           }
 
-          // Hash the new password
-          bcrypt.hash(newPassword, 10, (err, hashedPassword) => {
-            if (err) {
-              return res.status(500).send({ message: 'Error hashing password', error: err });
-            }
+//           // Hash the new password
+//           bcrypt.hash(newPassword, 10, (err, hashedPassword) => {
+//             if (err) {
+//               return res.status(500).send({ message: 'Error hashing password', error: err });
+//             }
 
-            // Update the user's password
-            user.password = hashedPassword;
-            user.save()
-              .then(() => {
-                // Delete the password reset token
-                resetToken.remove()
-                  .then(() => {
-                    res.status(200).send({ message: 'Password reset successful' });
-                  })
-                  .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
-              })
-              .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
-          });
-        })
-        .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
-    })
-    .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+//             // Update the user's password
+//             user.password = hashedPassword;
+//             user.save()
+//               .then(() => {
+//                 // Delete the password reset token
+//                 resetToken.remove()
+//                   .then(() => {
+//                     res.status(200).send({ message: 'Password reset successful' });
+//                   })
+//                   .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+//               })
+//               .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+//           });
+//         })
+//         .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+//     })
+//     .catch((err) => res.status(500).send({ message: 'Internal server error', error: err }));
+// };
+
+
+
+const resetPassword = async (req, res) => {
+  try {
+    const { email, otp, newPassword } = req.body;
+
+    // Find the user based on the email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send({ message: 'Invalid email or OTP' });
+    }
+
+    const resetToken = await PasswordResetToken.findOne({ user: user._id, otp });
+
+    if (!resetToken) {
+      return res.status(400).send({ message: 'Invalid OTP' });
+    }
+
+    if (resetToken.expiresAt < Date.now()) {
+      return res.status(400).send({ message: 'OTP has expired' });
+    }
+
+    // Remove the password reset token
+    await PasswordResetToken.deleteOne({ _id: resetToken._id });
+
+    // ... code to update the user's password ...
+
+    res.status(200).send({ message: 'Password reset successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error', error: error.message });
+  }
 };
 
 // Export the functions
